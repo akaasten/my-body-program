09d3ce5fc158190b69ca9d691039f8140176b178
Alex Kaasten <Akaasten@gmail.com>
2024-09-21 10:06:18 +0200

    [website] sitemap generator

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,28 @@ module.exports = {
   generateRobotsTxt: true,
   // use this to exclude routes from the sitemap (i.e. a user dashboard). By default, NextJS app router metadata files are excluded (https://nextjs.org/docs/app/api-reference/file-conventions/metadata)
   exclude: ["/twitter-image.*", "/opengraph-image.*", "/icon.*"],
+  additionalPaths: async (config) => {
+    // Générer des chemins dynamiques pour les articles
+    const fs = require('fs');
+    const path = require('path');
+
+    const articlePaths = [];
+
+    const articlesDirectory = path.join(__dirname, 'app', '[locale]', 'blog', '_assets', 'articles');
+    for (const locale of fs.readdirSync(articlesDirectory)) {
+      const categories = fs.readdirSync(path.join(articlesDirectory, locale));
+      for (const category of categories) {
+        const articles = fs.readdirSync(path.join(articlesDirectory, locale, category));
+        // Créer les URLs pour chaque article
+        const paths = articles.map((article) => {
+          return {
+            loc: `/${locale}/blog/${category}/${article.replace('.md', '')}`,
+            lastmod: new Date().toISOString(),
+          };
+        });
+        articlePaths.push(...paths); // Fix: push the generated paths into the articlePaths array
+      }
+    }
+    return articlePaths;
+  },
 };
